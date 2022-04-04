1ce22a5d87058548eaa994fd11e8eaf5e19b482c
21madanputlekar <60030300+21madanputlekar@users.noreply.github.com>
2022-04-04 17:21:46 +0530

    Add files via upload

---
+++ tb_halfsubtractor.v
@@ -0,0 +1,22 @@
+
+module tb_halfsubtractor;
+reg a,b;
+wire diff,borrow;
+
+half_subtractor UUT(.a(a),.b(b),.diff(diff),.borrow(borrow));
+initial begin
+$monitor($time,"a=%b b=%b diff=%b borrow=%b",a,b,diff,borrow);
+a=0;
+b=0;
+#10
+a=0;
+b=1;
+#10
+a=1;
+b=0;
+#10
+a=1;
+b=1;
+#10;
+end
+endmodule
